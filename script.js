e767d1773d0c7d665668167aa2a2cf43499df619
pekatet <e.pestereva@g.nsu.ru>
2022-03-03 18:41:09 +0700

    group in xml

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+const PERIOD_GRP_NAME="ОбщаяПродолжительностьСтраховогоСтажаНаДатуОбращенияГражданинаВОСЗ"
+
 function formToXml(form){
   var xmlInput=document.getElementById("query");
   var xmldata=['<?xml version="1.0" encoding="UTF-8"?>'];
-  xmldata.push("<element name=\"ЗАПРОС_СВЕДЕНИЙ_О_СТРАХОВОМ_СТАЖЕ\">");
+  xmldata.push("<ЗАПРОС_СВЕДЕНИЙ_О_СТРАХОВОМ_СТАЖЕ>");
   
   var inputs=form.elements;
   error = false;
@@ -12,26 +14,42 @@ function formToXml(form){
         error = true;
       }
       var element=document.createElement(inputs[i].name);
-      element.appendChild(document.createTextNode(inputs[i].value))
+      element.appendChild(document.createTextNode(inputs[i].value));
       xmldata.push(element.outerHTML);
     }
+    i++;
   }
-
-
-  xmldata.push("</element>");
+  var periodGroup=document.createElement(PERIOD_GRP_NAME);
+  while(inputs[i].id.includes('period')){
+    if (inputs[i].name){
+      if(!checkValidity(element = inputs[i])){
+        error = true;
+      }
+      var element=document.createElement(inputs[i].name);
+      element.appendChild(document.createTextNode(inputs[i].value));
+      periodGroup.appendChild(element);
+    }
+    i++;
+  }
+  xmldata.push(periodGroup.outerHTML)
+  xmldata.push("</ЗАПРОС_СВЕДЕНИЙ_О_СТРАХОВОМ_СТАЖЕ>");
   console.log(xmldata);
-  //xmlInput.setAttribute('value', xmldata.join("\r\n"));
-  xmlInput.value = xmldata.join("\n")
-  //return xmldata.join("\n");
+  if(!error){
+    xmlInput.value = xmldata.join("\n");
+  }
+  else{
+    xmlInput.value='';
+  }
 }
 
 function checkValidity(element){
   errorMessage = document.querySelector(`#${element.id}+span.error`);
-  if(!element.checkValidity){
+  
+  if(!element.checkValidity()){
     showError(element, errorMessage);
     return false;
   }
-  else{
+  else if(errorMessage !== null){
     errorMessage.textContent = ''; 
     errorMessage.className = 'error';
   }
@@ -39,13 +57,14 @@ function checkValidity(element){
 }
 
 function showError(element, errorMessage) {
+  console.log(errorMessage)
   label = document.querySelector(`label[for=${element.id}]`);
   if(element.validity.valueMissing) {
     errorMessage.textContent = `Поле ${label.innerHTML} обязательно для заполнения`;
-  } else if(email.validity.patternMismatch) {
+  } else if(element.validity.patternMismatch) {
     errorMessage.textContent = 'Пожалуйста, введите значение в формате '+element.placeholder;
   } else if(element.validity.rangeOverflow || element.validity.rangeUnderflow ) {
     errorMessage.textContent = `Пожалуйста, введите значение от ${element.min} до ${element.max}`;
   }
-  emailError.className = 'error active';
+  errorMessage.className = 'error active';
 }
